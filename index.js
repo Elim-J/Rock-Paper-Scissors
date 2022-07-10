82708f6e60b8066b699236b4847a67ee6e9f5d43
Sabrina Kramer <01CodePathways11@gmail.com>
2022-07-10 17:33:28 -0500

    Add pseudocode for game

---
+++ index.js
@@ -0,0 +1,25 @@
+/*player competes against a computer -- no interface for game, only console.log
+
+global array of choices ["rock", "paper", "scissors"]
+playAgain = false
+let computerOne;   
+let playerOne;
+
+function randomNum() returns random number 0-2
+function computerChoice() randomly chooses rock paper or scissors from global array for comp and returns choice
+function playerChoice() keeps prompting until player inputs correct option (rock, paper, or scissors)
+    make player input lowercase before checking correctness against global array
+function comparison() determines if player wins or not
+function playAgainPrompt() asks user if player would like to play again
+
+ask player if they want to play again -> play function and prompt inside of do-while loop?
+
+function playGame():
+    computer chooses option
+    player chooses option
+    determine end result
+
+
+playAgainPrompt()
+
+*/
