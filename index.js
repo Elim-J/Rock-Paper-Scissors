cb251b531eaa8d6b1c8568b39d9db7f1e3c62a5a
Sabrina Kramer <01CodePathways11@gmail.com>
2022-07-10 18:06:01 -0500

    Add random(), computerChoice(), and playerChoice()

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-/*player competes against a computer -- no interface for game, only console.log
-
-global array of choices ["rock", "paper", "scissors"]
-playAgain = false
+const handChoices = ["rock", "paper", "scissors"];
+let playAgain = false;
 let computerOne;   
 let playerOne;
 
-function randomNum() returns random number 0-2
-function computerChoice() randomly chooses rock paper or scissors from global array for comp and returns choice
-function playerChoice() keeps prompting until player inputs correct option (rock, paper, or scissors)
-    make player input lowercase before checking correctness against global array
+//Return number 0-2
+function random(){
+    return Math.floor((Math.random() * 3));
+}
+
+//computer chooses a random hand
+function computerChoice(){
+    return handChoices[random()];
+}
+
+function playerChoice(){
+    let userChoice;
+    
+    do{
+        
+        userChoice = prompt("Please choose rock, paper, or scissors");
+        userChoice = userChoice.toLowerCase();
+    }while( (userChoice !== handChoices[0]) &&
+            (userChoice !== handChoices[1]) &&
+            (userChoice !== handChoices[2]) );
+            
+    return userChoice;
+}
+
+/*
 function comparison() determines if player wins or not
 function playAgainPrompt() asks user if player would like to play again
 
